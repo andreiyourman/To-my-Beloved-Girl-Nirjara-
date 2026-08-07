5496b805c0c6debf08e5651ad5329ca6d0b494af
andreiyourman <andreimalakas2@gmail.com>
2026-08-08 02:29:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const btn = document.getElementById("giftBtn");
-
-btn.addEventListener("click", () => {
-    alert("Happy Birthday, My Beloved Girl Nirjara ❤️\n\nThe real surprise is coming soon... 🌅🌹");
-});
+function showMessage() {
+    document.getElementById("message").style.display = "block";
+}
